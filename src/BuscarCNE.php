4a0b679ee06e6bf836490fd73bcb800d52a7b10c
gestacio <gestacio1310@gmail.com>
2023-05-25 11:58:17 -0400

    arreglando algo de cóodigo

--- src/BuscarCNE.php
+++ src/BuscarCNE.php
@@ -3,25 +3,25 @@
 class BuscarCNE {
     public function obtenerElector($nacionalidad = "", $cedula = "") {
         $url = "http://www.cne.gob.ve/web/registro_electoral/ce.php?nacionalidad=".$nacionalidad."&cedula=".$cedula;
-        $ch = curl_init();
-        curl_setopt($ch,CURLOPT_URL, $url);
-        curl_setopt($ch,CURLOPT_REFERER,'http://www.cne.gob.ve/');
-        curl_setopt($ch,CURLOPT_USERAGENT,'Mozilla/5.0 (X11; Linux i686; rv:32.0) Gecko/20100101 Firefox/32.0');
-        curl_setopt($ch,CURLOPT_RETURNTRANSFER,TRUE);
-        curl_setopt($ch,CURLOPT_FRESH_CONNECT,TRUE);
-        curl_setopt($ch,CURLOPT_CONNECTTIMEOUT,5);
-        curl_setopt($ch,CURLOPT_TIMEOUT,10);
-        $html=curl_exec($ch);
+        $curlHandle = curl_init();
+        curl_setopt($curlHandle,CURLOPT_URL, $url);
+        curl_setopt($curlHandle,CURLOPT_REFERER,'http://www.cne.gob.ve/');
+        curl_setopt($curlHandle,CURLOPT_USERAGENT,'Mozilla/5.0 (X11; Linux i686; rv:32.0) Gecko/20100101 Firefox/32.0');
+        curl_setopt($curlHandle,CURLOPT_RETURNTRANSFER,TRUE);
+        curl_setopt($curlHandle,CURLOPT_FRESH_CONNECT,TRUE);
+        curl_setopt($curlHandle,CURLOPT_CONNECTTIMEOUT,5);
+        curl_setopt($curlHandle,CURLOPT_TIMEOUT,10);
+        $html=curl_exec($curlHandle);
         if($html==false){
-            $m=curl_error(($ch));
-            error_log($m);
-            curl_close($ch);
+            $errorMessage=curl_error(($curlHandle));
+            error_log($errorMessage);
+            curl_close($curlHandle);
             $json['error'] = true;
-            $json['descripcion'] = $m;
+            $json['descripcion'] = $errorMessage;
             print json_encode($json);
             return;
         } else {
-            curl_close($ch);
+            curl_close($curlHandle);
             if (strpos($html, '<b>DATOS DEL ELECTOR</b>') > 0) {
                 $modo = 1; # Puede Votar
             // } else if (strpos($html, '<strong>DATOS PERSONALES</strong>') > 0) {
